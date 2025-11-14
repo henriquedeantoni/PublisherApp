2834b26bfc752d95dc2f845fb43dfc95b9047d38
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-11-13 23:57:52 -0300

    feat: product service

--- src/services/ProductService.java
+++ src/services/ProductService.java
@@ -1,6 +1,21 @@
 package services;
 
+import java.util.List;
+
+import model.Product;
+
 public class ProductService {
 	
+	private final ApiClient apiClient = new ApiClient();
 	
+	public List<Product> getAllProducts() throws Exception {
+		String responseJson = apiClient.get("/products");
+		return JsonUtil.fromJsonList(responseJson, Product.class);
+	}
+	
+	public Product registerProduct(Product product) throws Exception{
+		String jsonBody = JsonUtil.toJson(product);
+		String response = apiClient.post("/products", jsonBody);
+		return JsonUtil.fromJson(response, Product.class);
+	}
 }
