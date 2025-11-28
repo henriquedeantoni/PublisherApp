a7c59e94762e8eb8093043fb8fed6411758414df
Henrique De Antoni <henriqdeantoni@gmail.com>
2025-11-27 23:59:33 -0300

    feat: new method

--- src/util/transformToHtml.java
+++ src/util/transformToHtml.java
@@ -13,4 +13,25 @@ public class transformToHtml {
 	public String textToLink(String refUrl, String text) {
 		return "<a href=" + "\"" + refUrl + "\"" + ">" + text + "</a>";
 	}
+	
+	public String textToLinkBlank(String refUrl, String text) {
+		return "<a href=" + "\"" + refUrl + "\"" + "target=\"_blank\"" + ">" + text + "</a>";
+	}
+	
+	public String linkToButton(String refUrl, String text) {
+		"<a href=\""+ refUrl + "\"" + "class=\"btn\">" + text + "</a>\r\n"
+		+ "\r\n"
+		+ "<!-- CSS -->\r\n"
+		+ "<style>\r\n"
+		+ "  .btn {\r\n"
+		+ "    display: inline-block;\r\n"
+		+ "    padding: 10px 16px;\r\n"
+		+ "    border-radius: 6px;\r\n"
+		+ "    text-decoration: none;\r\n"
+		+ "    font-weight: 600;\r\n"
+		+ "    border: 1px solid #333;\r\n"
+		+ "  }\r\n"
+		+ "  .btn:hover { opacity: 0.9; }\r\n"
+		+ "</style>"
+	}
 }
